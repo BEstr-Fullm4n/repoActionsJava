dd39d5766d158046d7412029c0178005c7b03f6f
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-01-27 17:16:21 -0600

    Changes using Code Review and publish reports resources in xml

--- src/main/java/com/bancoppel/gitactions/HelloController.java
+++ src/main/java/com/bancoppel/gitactions/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello World 4 GitHub-Actions v1.2";
+        return "Hello World 4 GitHub-Actions v1.3";
     }
 }
