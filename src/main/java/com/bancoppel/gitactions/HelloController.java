aeac3e504a63a3076dcd6bbb273dac73d9fb9d41
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-01-27 17:04:49 -0600

    Changes to test copilot using code review and maven

--- src/main/java/com/bancoppel/gitactions/HelloController.java
+++ src/main/java/com/bancoppel/gitactions/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello World 4 GitHub-Actions v1.1";
+        return "Hello World 4 GitHub-Actions v1.2";
     }
 }
