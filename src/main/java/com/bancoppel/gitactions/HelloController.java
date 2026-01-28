e63d45f9f5385de9f3b2610f70b21f6aa59930f5
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-01-27 18:39:13 -0600

    Changes using new branch to test pull request and github actions v2

--- src/main/java/com/bancoppel/gitactions/HelloController.java
+++ src/main/java/com/bancoppel/gitactions/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello World 4 GitHub-Actions v1.4 Test-pullReqest";
+        return "Hello World 4 GitHub-Actions v1.4 Test-pullReqest 2";
     }
 }
